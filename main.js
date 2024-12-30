2eb4b8bc213b59ae45a6a3ccf815730782760318
anusb412 <anusb412@gmail.com>
2024-12-30 11:42:11 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
